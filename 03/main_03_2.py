6f5f98149750e03d2978fa17d12961068b0d3614
Tommy Duchesne <tommy.duchesne2@gmail.com>
2021-12-03 14:09:15 -0500

    03_2: put repetitive into functions

--- 03/main_03_2.py
+++ 03/main_03_2.py
@@ -8,8 +8,6 @@ with open(f"{os.path.dirname(__file__)}/input", mode='r') as file:
 
 # input_ = ["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
 
-bin_length = len(input_[0])
-
 
 def get_select_bit(lines, index, func):
     column = ""
@@ -21,20 +19,16 @@ def get_select_bit(lines, index, func):
     return func(number_of_ones, number_of_zeroes)
 
 
-oxygen_candidates = copy.copy(input_)
-for i in range(bin_length):
-    criteria_bit = get_select_bit(oxygen_candidates, i, lambda ones, zeroes: "1" if ones >= zeroes else "0")
-    oxygen_candidates = list(filter(lambda candidate_str: criteria_bit == candidate_str[i], oxygen_candidates))
-    if len(oxygen_candidates) == 1:
-        break
+def get_data(select_func):
+    candidates = copy.copy(input_)
+    for bit_index in range(len(input_[0])):
+        select_bit = get_select_bit(candidates, bit_index, select_func)
+        candidates = list(filter(lambda candidate_str: select_bit == candidate_str[bit_index], candidates))
+        if len(candidates) == 1:
+            break
+    return int(candidates[0], base=2)
 
-c02_candidates = copy.copy(input_)
-for i in range(bin_length):
-    criteria_bit = get_select_bit(c02_candidates, i, lambda ones, zeroes: "0" if ones >= zeroes else "1")
-    c02_candidates = list(filter(lambda candidate_str: criteria_bit == candidate_str[i], c02_candidates))
-    if len(c02_candidates) == 1:
-        break
 
-oxygen = int(oxygen_candidates[0], base=2)
-c02 = int(c02_candidates[0], base=2)
+oxygen = get_data(lambda ones, zeroes: "1" if ones >= zeroes else "0")
+c02 = get_data(lambda ones, zeroes: "0" if ones >= zeroes else "1")
 print(oxygen * c02)
